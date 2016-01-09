5ce51cb330417ee112b0857f5c34a1434e2cba28
shihuade <shihuadesysu@gmail.com>
2016-01-09 13:46:39 +0800

    add codecRTSimulator project

---
+++ codec/build/win10/CodecRTSimulator/Class1.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace CodecRTSimulator;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
+int Class1::Encode(){}int Class1::Decode(){}//Get encoder infofloat Class1::GetEncFPS(){}double Class1::GetEncTime(){}int  Class1::GetEncodedFrameNum(){}//get decoder infofloat Class1::GetDecFPS(){}double Class1::GetDecTime(){}int  Class1::GetDecodedFrameNum(){}
---
+++ codec/build/win10/CodecRTSimulator/Class1.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace CodecRTSimulator
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+
+		int Encode();		int Decode();		//Get encoder info		float GetEncFPS();		double GetEncTime();		int  GetEncodedFrameNum();		//get decoder info		float GetDecFPS();		double GetDecTime();		int  GetDecodedFrameNum();
+    };
+}
