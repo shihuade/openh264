365a4d6aba9ed3715bba59c0844ddc4555e523d2
shihuade <shihuadesysu@gmail.com>
2016-01-24 15:45:51 +0800

    add UI for C# project

--- codec/build/win10/CodecApp/MainPage.xaml.cs
+++ codec/build/win10/CodecApp/MainPage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using CodecRTSimulator;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace CodecApp
@@ -21,10 +21,64 @@ namespace CodecApp
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
-    {
+    {   private CodecRTComponent vRTCCodec;
         public MainPage()
         {
             this.InitializeComponent();
+            vRTCCodec =  new CodecRTComponent();
+        }
+
+        private void CallDecoder(object sender, RoutedEventArgs e)
+        {
+            int iRetVal = 0;
+            float fFPS = 0.0F;
+            double dDecoderTime = 0.0;
+            int iDecodedFrame = 0;
+
+            string  sDecoderInfo = "Decoder performance: \n";
+
+            iRetVal = vRTCCodec.Decode();
+
+            if (0 == iRetVal)
+            {
+                fFPS = vRTCCodec.GetDecFPS();
+                dDecoderTime = vRTCCodec.GetDecTime();
+                iDecodedFrame = vRTCCodec.GetDecodedFrameNum();
+                sDecoderInfo += "FPS : " + fFPS.ToString() + "\n";
+                sDecoderInfo += "DecTime(sec): " + dDecoderTime.ToString() + "\n";
+                sDecoderInfo += "DecodedFrameNum: " + iDecodedFrame.ToString() + "\n";
+                DecoderInfo.Text = sDecoderInfo;
+            }
+            else {
+                DecoderInfo.Text = "Decoded failed!...";
+            }
+
+        }
+
+        private void CallEncoder(object sender, RoutedEventArgs e)
+        {
+            int iRetVal = 0;
+            float fFPS = 0.0F;
+            double dEncoderTime = 0.0;
+            int iEncodedFrame = 0;
+            string  sEncoderInfo = "Encoder performance: \n";
+
+            iRetVal = vRTCCodec.Encode();
+
+            if (0 == iRetVal)
+            {
+                fFPS = vRTCCodec.GetEncFPS();
+                dEncoderTime = vRTCCodec.GetEncTime();
+                iEncodedFrame = vRTCCodec.GetEncodedFrameNum();
+                sEncoderInfo += "FPS: " + fFPS.ToString() + "\n";
+                sEncoderInfo += "EncTime(sec): " + dEncoderTime.ToString() + "\n";
+                sEncoderInfo += "EncodedFrameNum: " + iEncodedFrame.ToString() + "\n";
+                EncoderInfo.Text = sEncoderInfo;
+            }
+            else {
+                EncoderInfo.Text = "Encoded failed!...";
+            }
+
         }
     }
 }
