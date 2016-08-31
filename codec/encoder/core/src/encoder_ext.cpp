43ba56759dd563b8c1e034c604631873d815c0b6
huade <huashi@cisco.com>
2016-08-31 17:17:56 +0800

    removed sLayerInfo.pSliceInLayer and used pCurLayer->sSliceThreadInfo.pSliceInThread[0] for layer slice buffer

--- codec/encoder/core/src/encoder_ext.cpp
+++ codec/encoder/core/src/encoder_ext.cpp
@@ -902,8 +902,6 @@ int32_t   InitMbListD (sWelsEncCtx** ppCtx) {
 
 void FreeSliceInLayer (SDqLayer* pDq, CMemoryAlign* pMa) {
   int32_t iIdx = 0;
-
-  FreeSliceBuffer (pDq->sLayerInfo.pSliceInLayer, pDq->iMaxSliceNum, pMa, "pSliceInLayer");
   for (; iIdx < MAX_THREADS_NUM; iIdx ++) {
     FreeSliceBuffer (pDq->sSliceThreadInfo.pSliceInThread[iIdx],
                      pDq->sSliceThreadInfo.iMaxSliceNumInThread[iIdx],
